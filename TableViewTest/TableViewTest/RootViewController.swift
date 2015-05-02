0f363afe5f58189bafa9c5db29df6bb5a0d16182
csny <masaono_13@msn.com>
2015-05-02 18:42:11 +0900

    コメントとか修正

--- TableViewTest/TableViewTest/RootViewController.swift
+++ TableViewTest/TableViewTest/RootViewController.swift
@@ -8,7 +8,8 @@
 
 import UIKit
 
-struct strctText {
+// セルのラベルに出力する変数
+struct strctCellLabel {
     var bunsyou: String
     var hiduke: NSDate?
 }
@@ -17,7 +18,7 @@ class RootViewController: UITableViewController {
     
     // インスタンス変数
     let textPoolArr: Array<String> = ["Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras sodales diam sed turpis mattis dictum. In laoreet porta eleifend. Ut eu nibh sit amet est iaculis faucibus.","initWithBitmapDataPlanes:pixelsWide:pixelsHigh:bitsPerSample:samplesPerPixel:hasAlpha:isPlanar:colorSpaceName:bitmapFormat:bytesPerRow:bitsPerPixel:", "祇辻飴葛蛸鯖鰯噌庖箸", "Nam in vehicula mi.", "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.", "あのイーハトーヴォの\nすきとおった風、\n夏でも底に冷たさをもつ青いそら、\nうつくしい森で飾られたモーリオ市、\n郊外のぎらぎらひかる草の波。"]
-    var outputArr:[strctText]=[]
+    var outputArr:[strctCellLabel]=[]
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -29,8 +30,8 @@ class RootViewController: UITableViewController {
         
         // UITableViewAutomaticDimensionはiOS8から
         // セルの高さの計算は Autolayout に任せる
-        tableView.rowHeight = UITableViewAutomaticDimension;
-        tableView.estimatedRowHeight = 40.0;
+        tableView.rowHeight = UITableViewAutomaticDimension
+        tableView.estimatedRowHeight = 40.0
     }
 
     override func didReceiveMemoryWarning() {
@@ -44,7 +45,7 @@ class RootViewController: UITableViewController {
         var dataIndex: Int = Int(arc4random() % UInt32(textPoolArr.count))
         let str: String = textPoolArr[dataIndex]
         let date = NSDate()
-        var tempstrct = (strctText(bunsyou: str, hiduke: date))
+        var tempstrct = (strctCellLabel(bunsyou: str, hiduke: date))
         // データ挿入
         outputArr.insert(tempstrct, atIndex: 0)
         
@@ -87,6 +88,7 @@ class RootViewController: UITableViewController {
     // editモード時のボタンと動作
     override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
         if (editingStyle == UITableViewCellEditingStyle.Delete) {
+            // 配列の要素削除と表示セルの削除
             outputArr.removeAtIndex(indexPath.row)
             tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
         }
